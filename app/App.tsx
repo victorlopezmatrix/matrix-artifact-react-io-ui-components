6cf0d08ce928dbd6f6755a08b14d94b9ae7e4a95
essenwangermatrix <victor.essenwanger.matrix@gmail.com>
2023-10-19 17:57:15 -0500

    feat: add app with react native cli

---
+++ app/App.tsx
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+} from 'react-native';
+import {Title} from 'matrix-artifact-react-io-ui-components'
+
+function App(): JSX.Element {
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Title>Hello world</Title>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,11 @@
+import React from "react";
+import { Text } from "react-native";
+import type {TextProps} from "react-native";
+
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
+}
+
+export const Title: React.FC<TextProps> = (props) => {
+  return <Text {...props}/>;
 }
